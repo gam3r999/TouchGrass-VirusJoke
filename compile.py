49b2607db9db0ee6bdacb46e24b2b04f2182d190
gam3r999 <naumoffmanasseh@gmail.com>
2026-02-27 21:00:24 -0500

    Add files via upload

---
+++ compile.py
@@ -0,0 +1,28 @@
+import PyInstaller.__main__
+import os
+
+# Ensure these match your actual filenames
+script_name = "videobsod.py"
+icon_file = "resources/appicon.ico"
+
+PyInstaller.__main__.run([
+    script_name,
+    '--onefile',
+    '--noconsole',
+    f'--icon={icon_file}',
+    '--uac-admin',               # This forces the 🛡️ Admin Shield icon
+    '--add-data=resources;resources',
+    
+    # Brute force metadata collection
+    '--copy-metadata=imageio',
+    '--copy-metadata=moviepy',
+    '--copy-metadata=tqdm',      # moviepy often needs this too
+    '--collect-all=imageio',
+    '--collect-all=moviepy',
+    
+    '--name=Legit',
+    '--clean',
+    '-y'
+])
+
+print("\n[SUCCESS] Build finished. Check the 'dist' folder.")
